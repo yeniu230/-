c982c1d6632d2d2eea9ed6ec2723eaa7ea0844ad
Niu ye <yeniu230@qq.com>
2017-12-29 23:31:31 +0800

    创建随机数组并赋予到表格中

--- game.js
+++ game.js
@@ -1,11 +1,23 @@
-let num = [];
-for(let i = 1; i <= 8; i++) {
+var num = [];
+for(var i = 1; i <= 8; i++) {
     num.push(i);
 }//创建一个数字1-8的数组
-let samenum = num;//复制数组
-let double = num.concat(samenum);//使数组内分别包含两个1-8
+var sameNum = num;//复制数组
+var double = num.concat(sameNum);//使数组内分别包含两个1-8
 //将double数组随机排列
-let td = document.getElementsByTagName("td");
-for(let x = 0; x < 16; x++) {
-    td[x].innerText = double[x];
+Array.prototype.shuffle = function() {
+    var input = this;
+    for (var i = input.length-1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random()*(i+1));
+        var itemAtIndex = input[randomIndex];
+        input[randomIndex] = input[i];
+        input[i] = itemAtIndex;
+    }
+    return input;
+}
+var bt = document.getElementsByClassName('button1');
+var td = document.getElementsByTagName('td');
+var randomArray = double.shuffle();
+for(var x = 0; x < 16; x++) {
+    td[x].innerText = randomArray[x];
 }
